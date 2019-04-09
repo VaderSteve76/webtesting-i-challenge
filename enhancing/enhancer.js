7583ade8a82ae44dc5de7d00745d3f4ed6a4fe2d
VaderSteve76 <alverson.s@yahoo.com>
2019-04-08 22:04:42 -0400

    enhancer

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,49 @@
 module.exports = {
-  succeed,
+  success,
   fail,
-  repair,
-  get,
-};
+  repair
+}
 
-function succeed(item) {
-  return { ...item };
+
+function success(item) {
+  const newEnhancement = item.enhancement + 1;
+  const newName = item.originalName;
+    return {
+      ...item,
+      enhancement: newEnhancement,
+      name: `[+${newEnhancement}] ${newName}`
+    };
 }
 
 function fail(item) {
-  return { ...item };
+  const itemEnhancement = item.enhancement;
+  const itemDurability = item.durability;
+  let newDurability = itemDurability;
+  let newEnhancement = itemEnhancement;
+  const newName = item.originalName;
+
+  if(itemEnhancement > -1 && itemEnhancement < 15) {
+    newDurability = itemDurability - 5;
+  } else if(itemEnhancement > 14) {
+    newDurability = itemDurability - 10;
+  } else if(itemEnhancement > 16) {
+    newEnhancement = itemEnhancement - 1;
+  }
+
+  if(newEnhancement <= 14 && newDurability < 25)
+    return "Enhancement Failed";
+
+  if(newEnhancement >= 15 && newDurability < 10)
+    return "Enhancement Failed";
+
+    return {
+      ...item,
+      name: `[+${newEnhancement}] ${newName}`,
+      durability: newDurability,
+      enhancement: newEnhancement
+    }
 }
 
 function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
+  return {...item, durability: 100};
 }
